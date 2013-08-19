f29af9ea175fdde0d5179d26d4203692da05a82c
Jonas <jonjts@gmail.com>
2013-08-18 22:55:12 -0300

    Adicionado tela de Emprestimo

---
+++ app/src/br/com/tep/mystuff/CadastrarEmprestimo.java
@@ -0,0 +1,15 @@
+package br.com.tep.mystuff;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class CadastrarEmprestimo extends SherlockActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_emprestimo);
+	}
+
+}
